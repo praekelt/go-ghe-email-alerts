de94c41be04fb77b5f65cd0e32432cf6661ed4ca
dbreedt <dbreedt@retrorabbit.co.za>
2016-07-26 11:59:50 +0200

    Made the config just json

--- config.js
+++ config.js
@@ -1,13 +1,17 @@
-module.exports.config = {
-    keywords: [
-        'test',
-        'test2'
-    ],
+{
+  "keywords": [
+    "test",
+    "test2"
+  ],
 
-    welcome_message: 'Lorem Ipsum',
+  "welcome_message": "Lorem Ipsum",
 
-    email_config: {
-        to: 'test@test.net',
-        template: '23492304902934'
-    }
+  "email_config": {
+    "to": "test@test.net",
+    "from": "test@test.net",
+    "from_name": "test",
+    "subject": "keyword detected"
+    "api_key": "12341234134",
+    "template": "23492304902934"
+  }
 };
