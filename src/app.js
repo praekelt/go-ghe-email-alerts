9fee5044b041ffda2e59f1186d9213488d676ebb
dbreedt <dbreedt@retrorabbit.co.za>
2016-07-28 21:25:08 +0200

    Renamed utils references to go.utils

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ go.app = function () {
   var vumigo = require('vumigo_v02');
   var App = vumigo.App;
   var EndState = vumigo.states.EndState;
-  var utils = require('./utils');
   var Q = require('q');
 
   var GoApp = App.extend(function (self) {
@@ -10,8 +9,8 @@ go.app = function () {
 
     self.states.add('states:start', function (name) {
       var config = self.im.config;
-      var found = utils.process_keywords(self.im.msg, config.keywords);
-      var usr_addr = utils.format_address(self.im.msg.from_addr);
+      var found = go.utils.process_keywords(self.im.msg, config.keywords);
+      var usr_addr = go.utils.format_address(self.im.msg.from_addr);
       var state = new EndState(name, {
         text: config.welcome_message,
         next: 'states:start'
@@ -19,7 +18,7 @@ go.app = function () {
 
       return Q.promise(function (resolve, reject) {
         if (found) {
-          return utils.send_email(config.email_config, usr_addr, self.im)
+          return go.utils.send_email(config.email_config, usr_addr, self.im)
             .then(function () {
               return resolve(state);
             });
